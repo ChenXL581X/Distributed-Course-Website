e5e8b2acdc99c9f48aa7c7c44649e0d28f0ef7d8
faraway <625473164@qq.com>
2015-10-26 19:13:08 +0800

    web-ace框架和登录注册页面

---
+++ HQC/includes/header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<title>Login Page - Ace Admin</title>
+
+		<meta name="description" content="User login page" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+		<!--basic styles-->
+
+		<link href="ace/assets/css/bootstrap.min.css" rel="stylesheet" />
+		<link href="ace/assets/css/bootstrap-responsive.min.css" rel="stylesheet" />
+		<link rel="stylesheet" href="ace/assets/css/font-awesome.min.css" />
+
+		<!--[if IE 7]>
+		  <link rel="stylesheet" href="assets/css/font-awesome-ie7.min.css" />
+		<![endif]-->
+
+		<!--page specific plugin styles-->
+
+		<!--fonts-->
+
+		<link rel="stylesheet" href="ace/assets/css/ace-fonts.css" />
+
+		<!--ace styles-->
+
+		<link rel="stylesheet" href="ace/assets/css/ace.min.css" />
+		<link rel="stylesheet" href="ace/assets/css/ace-responsive.min.css" />
+		<link rel="stylesheet" href="ace/assets/css/ace-skins.min.css" />
+		
+		<!--[if lte IE 8]>
+		  <link rel="stylesheet" href="assets/css/ace-ie.min.css" />
+		<![endif]-->
+
+		<!--inline styles related to this page-->
+		<link rel="stylesheet" type="text/css" href="css/custom.css">
+	</head>
