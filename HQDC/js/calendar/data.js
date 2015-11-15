61558e3155b6f10db3a314e36422869af36440cf
faraway <625473164@qq.com>
2015-11-15 17:07:15 +0800

    css

--- HQDC/js/calendar/data.js
+++ HQDC/js/calendar/data.js
@@ -3,7 +3,7 @@ var codropsEvents = {
 	// '11-13-2015' : '<a href="http://tympanus.net/codrops/2012/01/04/thumbnail-proximity-effect/">Thumbnail Proximity Effect with jQuery and CSS3</a>',
 	// '11-14-2015' : '<a href="http://tympanus.net/codrops/2012/01/02/fullscreen-background-image-slideshow-with-css3/">Fullscreen Background Image Slideshow with CSS3</a>',
 	// '11-12-2015' : '<span>Christmas Day</span>',
-	'11-12-2015' : '<span>New Year\'s Eve</span> <span>作业1作业1作业1作业1作业1作业1作业1作业1</span>',
+	'11-12-2015' : '<span>New Year\'s Eve</span> <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</span>',
 	'11-02-2015' : '<span>New Year\'s Eve</span>',
 	'11-04-2015' : '<a href="#">this is a test </a> <a href="#">this is a test </a>'
 };
