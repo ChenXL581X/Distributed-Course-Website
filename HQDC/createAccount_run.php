909650e7ac9262ecb93af636deb1914324fc3f7f
chenxiaolei123 <chen975675159@hotmail.com>
2015-11-21 17:55:25 +0800

    导入账号

--- HQDC/createAccount_run.php
+++ HQDC/createAccount_run.php
@@ -62,52 +62,31 @@ if(Input::exists('post')){
 	    // 	        之后调用user中的create函数循环创建账号
 	    //    显示不能创建的账号。
 	    if ($_POST["submitreg"] == 'fromExcel') {
-// 	        echo "hello world";
 	        $excel = new Excel();
 	        //可以设置保存文件的路径默认为 $path = './upfile/Excel';
 	        // $excel->setPath($path);
-	        
-	        //将文件存储到服务器，不然无法读取
-// 	        $filename = $excel->storeExcel();
-	        $resultArr = $excel->importExcel();
-	        print_r($resultArr);
 
-// 	        if (! empty ( $_FILES ['file_stu'] ['name'] ))
-// 	        {
-// 	            $tmp_file = $_FILES ['file_stu'] ['tmp_name'];
-// 	            $file_types = explode ( ".", $_FILES ['file_stu'] ['name'] );
-// 	            $file_type = $file_types [count ( $file_types ) - 1];
-// 	            /*�б��ǲ���.xls�ļ����б��ǲ���excel�ļ�*/
-// 	            if (strtolower ( $file_type ) != "xlsx")
-// 	            {
-// 	                echo  "�����ϴ�";
-// 	            }
-// 	            /*�����ϴ�·��*/
-// 	            $savePath = './upfile/';
-// 	            /*��ʱ���������ϴ����ļ�*/
-// 	            $str = date ( 'Ymdhis' );
-// 	            $file_name = $str . "." . $file_type;
-// 	            /*�Ƿ��ϴ��ɹ�*/
-// 	            if (! copy ( $tmp_file, $savePath . $file_name ))
-// 	            {
-// 	                echo "error";
-// 	            }
-// 	            /*
-// 	             *���ϴ���Excel���ݽ��д������ɱ������,����������������������ExcelToArray����
-// 	             ע�⣺�������ִ���˵������������read��������Excelת��Ϊ���鲢���ظ�$res,�ٽ������ݿ�д��
-// 	             */
-	        
-// 	            $objReader = PHPExcel_IOFactory::createReader('Excel2007');
-// 	            $objPHPExcel = PHPExcel_IOFactory::load($savePath . $file_name);
-// 	            $sheet = $objPHPExcel->getSheet(0);
-// 	            $highestRow = $sheet->getHighestRow(); // ȡ��������
-// 	            $highestColumn = $sheet->getHighestColumn(); // ȡ��������
-// 	            for ($j = 2; $j <= $highestRow; $j++)
-// 	                for ($k = 'A'; $k <= $highestColumn; $k++) {
-// 	                    $str = iconv('utf-8', 'gbk', $objPHPExcel->getActiveSheet()->getCell("$k$j")->getValue()). '\\';
-// 	                    echo $str;
-// 	                }
-// 	        }
+	        $resultArr = $excel->importExcel();
+
+	        $user = new User();
+	        $salt = Hash::salt(32);
+
+	        foreach ($$resultArr as $value) {
+	        	try{
+	        		$user->create(array(
+	        			'username' => $value[1],
+	        			'password' => $value[1],
+	        			'salt' => $sqlt,
+	        			'name' => $value[2],
+	        			'joined' => date('Y-m-d H:i:s'),
+	        			'group' => $value[3]
+	        			));
+	        		Session::flash('home' , 'create {$value[1]} account successfully');
+	        	}catch(Exception $e) {
+	        		die($e->getMessage());
+	        	}
+	        }
+
 	    }
 	    
 
