dcd570535cd4e9ec0b240f7900a27222664f92aa
faraway <625473164@qq.com>
2016-01-08 21:30:35 +0800

    courseIntroduce

--- HQDC/courseIntroduce.php
+++ HQDC/courseIntroduce.php
@@ -1,27 +1,134 @@
 <?php
 include 'includes/header.php';
 ?>
+<link rel="stylesheet" type="text/css" href="css/courseIntroduce.css">
 <div class="main row">
-	<div class="col-md-8 col-md-offset-2">
-	
-	<div id="courseIntroduce" class="">
-	<h1>课程简介</h1>
-	<?php 
-	   $courseInfo = new CourseInfo();
-	   echo $courseInfo->getCourseIntroduce();
-	?>
+	<div class="col-md-10 col-md-offset-1 row">
+	<div class="left col-md-8">
+		<ul class="nav nav-tabs">
+		  <li role="presentation" class="active"><a class="to-tab1" href="#">课程简介</a></li>
+		  <li role="presentation"><a class="to-tab2" href="#">教学环境</a></li>
+		  <li role="presentation"><a class="to-tab3" href="#">Messages</a></li>
+		</ul>
+		<div id="courseIntroduce" class="tab1">
+		<h1>课程简介</h1>
+		<div class="info">
+			<?php 
+			   $courseInfo = new CourseInfo();
+			   echo $courseInfo->getCourseIntroduce();
+			?>	
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		</div>
+		
+
+		</div>
+		
+		<div id="teachingEnvironment" class="tab2"> 
+		<h1>教学环境</h1>
+		<div class="info">
+			<?php 
+			   echo $courseInfo->getTeachingEnvironment();
+			?>	
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		</div>
+		</div>
+		<div class="tab3">
+			<div class="info">
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.	
+			</div>
+			
+		</div>
+		
+		
+	</div>
+	<div class="right col-md-4">
+		<div class="notice-box notice ">
+				
+			<div class="panel panel-default">
+			  <div class="panel-heading">最近通知</div>
+			  <div class="panel-body">
+			    <ul>
+			       <?php 
+		                $news = new News();
+		                $record = $news->getInfo();
+		                $count = count($record);
+		                foreach ($record as $value) {
+		                    $value = get_object_vars($value);
+		                    echo "<li><a href='newsDetail.php?id=".$value['id']."' target='_blank'>".$value['title']."</a>";
+		                    echo "<p>".$value['upload_time']."  <a href=''>".$value['upload_people']."</a></p>";
+		                    echo "</li>";
+		                }
+		                
+		            ?>
+<!-- 					    	<li> -->
+<!-- 					    		<a href="#" target="_blank">各位领导，各位老师，关于学院国庆、中秋假期放假安全稳定工作的...</a> -->
+<!-- 					    		<p>2015年3月3日11:00 <a href="#">马锐</a></p> -->
+<!-- 					    	</li> -->
+<!-- 					    	<li> -->
+<!-- 					    		<a href="#" target="_blank">请各位同学将作业发到以下邮箱...</a> -->
+<!-- 					    		<p>2015年3月3日11:00 <a href="#">马锐</a></p> -->
+<!-- 					    	</li> -->
+			    </ul>
+			  </div>
+			</div>
+
+			
+		</div>
+		<div class="notice-box  state  ">
+			
+
+			<div class="panel panel-default">
+			  <div class="panel-heading">最新动态</div>
+			  <div class="panel-body">
+			    <ul>
+			    <?php 
+			       $notice = new Notice();
+			       $record = $notice->getInfo();
+			       $count = count($record);
+			       foreach ($record as $value) {
+			           $value = get_object_vars($value);
+			           echo "<li><a href='newsDetail.php?id=".$value['id']."' target='_blank'>".$value['title']."</a>";
+			           echo "<p>".$value['upload_time']."  <a href=''>".$value['upload_people']."</a></p>";
+			           echo "</li>";
+			       }
+			    ?>
+					    	<!-- <li>
+					    		<a href="#" target="_blank">各位领导，各位老师，关于学院国庆、中秋假期放假安全稳定工作的...</a>
+					    		<p>2015年3月3日11:00 <a href="#">马锐</a></p>
+					    	</li> -->
+			    </ul>
+			  </div>
+			</div>
+		</div>
+		
 	</div>
 	
-	<div id="teachingEnvironment" class=""> 
-	<h1>教学环境</h1>
-	<?php 
-	   echo $courseInfo->getTeachingEnvironment();
-	?>
-	</div>
 	
 	
 	</div>
 </div>
+<script type="text/javascript" src="js/courseIntroduce.js"></script>
 <?php
 include "includes/footer.php";
 ?>
