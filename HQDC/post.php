a15f49582a2049ae57f5c06efa1f1e6abda59596
ChenXL581X <975675159@qq.com>
2016-01-15 15:57:42 +0800

    post修改

--- HQDC/post.php
+++ HQDC/post.php
@@ -4,7 +4,11 @@ $post = new Post();
 $postId = Input::get('postId');
 if(!$postId){
 	Session::flash('forum',"帖子不存在");
-	Redirect::to('forum.php');
+echo "<script type='text/javascript'>
+	
+		window.location.href = 'forum.php';
+		
+		</script>";
 }else{
 	$postData = $post->find($postId)->data();
 
@@ -145,7 +149,7 @@ if(!$postId){
 								<a class="show" href="#">展开图片</a>
 							</div>
 
-							<?
+							<?php
 								$imgArray = explode("|", $value->imgs);
 							}else{
 								$imgArray = false;
